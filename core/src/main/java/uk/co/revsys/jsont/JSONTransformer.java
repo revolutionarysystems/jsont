624045bd5fc27239ed22e608d0dd74b929977fa2
Adam Knight <adam.knight@opensquare.co.uk>
2014-06-26 14:51:56 +0100

    Allow for null values and recursive templates

--- core/src/main/java/uk/co/revsys/jsont/JSONTransformer.java
+++ core/src/main/java/uk/co/revsys/jsont/JSONTransformer.java
@@ -11,41 +11,90 @@ import org.json.JSONObject;
 
 public class JSONTransformer {
 
-    public String transformFromFile(String source, File transform, Map parameters) throws IOException{
+    public String transformFromFile(String source, File transform, Map parameters) throws IOException {
         return transform(source, FileUtils.readFileToString(transform), parameters);
     }
-    
-    public String transform(String source, String transform, Map parameters){
+
+    public String transform(String source, String transform, Map parameters) {
         JSONObject transformJSON = new JSONObject(transform);
+        Object key = transformJSON.keySet().toArray()[0];
+        String jpath = (String) key;
+        return transform(source, transformJSON, jpath, parameters);
+    }
+
+    private String transform(String source, JSONObject transformJSON, String jpath, Map parameters) {
+        String template;
+        Object templateObject = transformJSON.get(jpath);
+        if (templateObject instanceof String) {
+            template = "\"" + transformJSON.get(jpath).toString() + "\"";
+        } else {
+            template = transformJSON.get(jpath).toString();
+        }
         StringBuilder result = new StringBuilder();
-        for(Object key: transformJSON.keySet()){
-            String jpath = (String)key;
-            String template = transformJSON.get(jpath).toString();
-            List<String> matches = new LinkedList();
-            Object jmatch = JsonPath.read(source, jpath);
-            if(jmatch instanceof net.minidev.json.JSONObject){
-                matches.add(jmatch.toString());
-            }else{
-                matches = (List)jmatch;
-            }
-            for(String match: matches){
-                String partial = template;
-                while(partial.contains("{{")){
-                    String placeholder = partial.substring(partial.indexOf("{{")+2);
-                    placeholder = placeholder.substring(0, placeholder.indexOf("}}"));
+        List<String> matches = new LinkedList();
+        Object jmatch = JsonPath.read(source, jpath);
+        if (jmatch instanceof net.minidev.json.JSONObject) {
+            matches.add(jmatch.toString());
+        } else {
+            matches = (List) jmatch;
+        }
+        for (String match : matches) {
+            String partial = template;
+            System.out.println("partial = " + partial);
+            while (partial.contains("{{")) {
+                boolean hasQuoteAtStart = false;
+                boolean hasQuoteAtEnd = false;
+                boolean wrappedInQuotes = false;
+                String prefix = "{{";
+                String suffix = "}}";
+                int startIndex = partial.indexOf("{{");
+                if (startIndex > 0 && partial.charAt(startIndex - 1) == '\"') {
+                    hasQuoteAtStart = true;
+                }
+                String placeholder = partial.substring(startIndex + 2);
+                int endIndex = placeholder.indexOf("}}");
+                if (endIndex < placeholder.length() && placeholder.charAt(endIndex + 2) == '\"') {
+                    hasQuoteAtEnd = true;
+                    wrappedInQuotes = hasQuoteAtStart;
+                    if (wrappedInQuotes) {
+                        prefix = "\"{{";
+                        suffix = "}}\"";
+                    }
+                }
+                placeholder = placeholder.substring(0, endIndex);
+                System.out.println("placeholder = " + placeholder);
+                String replacementString;
+                if (transformJSON.has(placeholder)) {
+                    replacementString = transform(source, transformJSON, placeholder, parameters);
+                } else {
                     Object replacement = JsonPath.read(match, placeholder);
-                    partial = partial.replace("{{" + placeholder + "}}", replacement.toString());
+                    replacementString = parseObjectToString(replacement, wrappedInQuotes);
                 }
-                while(partial.contains("${")){
-                    String placeholder = partial.substring(partial.indexOf("${")+2);
-                    placeholder = placeholder.substring(0, placeholder.indexOf("}"));
-                    Object replacement = parameters.get(placeholder);
-                    partial = partial.replace("${" + placeholder + "}", replacement.toString());
-                }
-                result.append(partial);
+                partial = partial.replace(prefix + placeholder + suffix, replacementString);
             }
+            while (partial.contains("\"${")) {
+                String placeholder = partial.substring(partial.indexOf("\"${") + 3);
+                placeholder = placeholder.substring(0, placeholder.indexOf("}\""));
+                Object replacement = parameters.get(placeholder);
+                partial = partial.replace("\"${" + placeholder + "}\"", parseObjectToString(replacement, true));
+            }
+            result.append(partial);
         }
         return result.toString();
     }
-    
+
+    private String parseObjectToString(Object object, boolean wrapInQuotes) {
+        if (object == null) {
+            return "null";
+        } else if (object instanceof String) {
+            if (wrapInQuotes) {
+                return "\"" + object.toString() + "\"";
+            } else {
+                return object.toString();
+            }
+        } else {
+            return object.toString();
+        }
+    }
+
 }
--- core/src/test/java/uk/co/revsys/jsont/JSONTransformerTest.java
+++ core/src/test/java/uk/co/revsys/jsont/JSONTransformerTest.java
@@ -37,13 +37,18 @@ public class JSONTransformerTest {
      */
     @Test
     public void testTransform() {
-        String source = "{\"accountName\": \"Test Account\", \"user\": {\"firstName\": \"Test\", \"surname\": \"User\"}}";
-        String transform = "{\"$\": {\"name\": \"{{$.accountName}}\", \"id\": \"${id}\"}}";
+        String source = "{\"accountName\": \"Test Account\", \"accountId\": 9876, \"user\": {\"firstName\": \"Test\", \"surname\": \"User\"}}";
+        String transform = "{\"$\": {\"name\": \"{{$.accountName}}\", \"id\": \"{{$.accountId}}\", \"userid\": \"${userid}\", \"p1\": \"{{$.p1}}\", \"p2\": \"${p2}\"}}";
         JSONTransformer instance = new JSONTransformer();
-        String expResult = "{\"name\": \"Test Account\", \"id\": \"1234\"}";
+        String expResult = "{\"name\": \"Test Account\", \"id\": 9876, \"userid\": \"1234\", \"p1\": null, \"p2\": null}";
         Map parameters = new HashMap();
-        parameters.put("id", "1234");
+        parameters.put("userid", "1234");
         String result = instance.transform(source, transform, parameters);
+        assertEquals(new JSONObject(expResult).toString(), new JSONObject(result).toString());
+        transform = "{\"$\": {\"accountName\": \"{{$.accountName}} 1\", \"user\": \"{{$.user}}\"}, \"$.user\": {\"id\": \"${userid}\", \"name\": \"{{firstName}} {{surname}}\"}}";
+        expResult = "{\"accountName\": \"Test Account 1\", \"user\": {\"id\": \"1234\", \"name\": \"Test User\"}}";
+        result = instance.transform(source, transform, parameters);
+        System.out.println("result = " + result);
         assertEquals(new JSONObject(expResult).toString(), new JSONObject(result).toString());
         transform = "{\"$.user\": \"{{$}}\"}";
         expResult = "{\"firstName\": \"Test\", \"surname\": \"User\"}";
